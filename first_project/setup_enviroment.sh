1a14981ca9d2b8b684eb8a2c9c7152dfe800eb00
gasstin <gastonl1994@gmail.com>
2023-04-21 09:27:07 -0300

    add requirements.txt

--- first_project/setup_enviroment.sh
+++ first_project/setup_enviroment.sh
@@ -1,5 +1,4 @@
-curl -fsSL https://get.pulumi.com | sh
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf awscliv2.zip
+apt install python3 -y
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+pip install -r requirements.txt
